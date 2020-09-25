f0bc9c4785c18a39819a8b2dd2800f758cb881c6
唯一 <fuwy@foxmail.com>
2020-09-26 02:03:38 +0800

    update

--- scripts/module/background/module.js
+++ scripts/module/background/module.js
@@ -5,7 +5,7 @@ App.module.extend('background', function() {
     //
     let self = this;
 
-    this.init = function() {
+    this._init = function() {
         // 打开主界面
         chrome.browserAction.onClicked.addListener(function (tab) {
             chrome.tabs.create({url: chrome.extension.getURL("index.html")});
